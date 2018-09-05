f5a234bdc7b3b0b2f05ee0dfa6b70b528a5f6266
Jul Jen <juliajen.prog@gmail.com>
2018-09-05 20:47:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const driversByName = function (drivers) {
 
 // Receives an array of driver objects and returns the sum of the revenue earned by each driver
 const totalRevenue = function (drivers) {
-  return drivers.reduce( ( p, c ) => p + c, 0 );
+  return drivers.reduce( ( total, driver ) => total + driver.revenue, 0 );
   // return drivers.reduce( function (total, driver) {
   //   return total + driver.revenue; }, 0
   // );
